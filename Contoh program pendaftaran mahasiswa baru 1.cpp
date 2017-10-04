6f8b561d7c375a584210a976eff956d229af6085
AlfianHidayatM <32305314+AlfianHidayatM@users.noreply.github.com>
2017-10-04 18:51:18 +0700

    Menambahkan Deklarasinya

---
+++ Contoh program pendaftaran mahasiswa baru 1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <conio.h>
+#include <windows.h>
+
+using namespace std;
+int main(){
+int tes;
+float uan;
+
+cout<<" "<<endl;
+cout<<"\t ========================================================"<<endl;
+cout<<"\t SELEKSI MAHASISWA BARU UNIVERSITAS ISLAM KALIMANTAN"<<endl;
+cout<<"\t ========================================================"<<endl;
+ulang:;
+
+}
